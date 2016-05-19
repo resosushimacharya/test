f64883860dede7ef28c032d48722ff5416bbc83e
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-05-19 09:03:09 +0545

    fixed site_url

--- wp-content/themes/carpetcall/functions.php
+++ wp-content/themes/carpetcall/functions.php
@@ -6,8 +6,11 @@
 //show_admin_bar(false );
 
 
+//update_option('siteurl',"http://prakash.com/carpetcall");
+//update_option('home',"http://prakash.com/carpetcall");
 
-
+update_option('siteurl',"http://webdev.agileitsolutions.net/carpetcall/");
+update_option('home',"http://webdev.agileitsolutions.net/carpetcall/");
 
 include_once TEMPLATEPATH."/inc/carpetcall-script.php";
 //include_once TEMPLATEPATH."/inc/carpetcall-search.php";
