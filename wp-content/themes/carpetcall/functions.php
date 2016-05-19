f36d28e34f48a9d9d4c7997ef573666600adeb67
Chiranjivi Chaudhary <chiranjivi.chaudhary@aits.local>
2016-05-19 13:08:25 +0545

    changed update options

--- wp-content/themes/carpetcall/functions.php
+++ wp-content/themes/carpetcall/functions.php
@@ -5,8 +5,7 @@
 */
 //show_admin_bar(false );
 
-update_option('siteurl',"http://localhhost/carpetcall");
-update_option('home',"http://localhost/carpetcall");
+
 
 include_once TEMPLATEPATH."/inc/carpetcall-script.php";
 //include_once TEMPLATEPATH."/inc/carpetcall-search.php";
