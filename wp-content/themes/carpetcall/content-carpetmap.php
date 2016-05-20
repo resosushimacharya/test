e480e8f85e36fe7c6b35d2a7a496cc46bd042f92
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-05-20 12:29:22 +0545

    changes in home as well as creation of content page for about sec

---
+++ wp-content/themes/carpetcall/content-carpetmap.php
@@ -0,0 +1,28 @@
+<div class="container"><!-- about and map section start here -->
+       	<div class="abvist">
+        	<div class="row">
+            	<div class="col-md-6"><!-- about start here -->
+                <div class="intro">
+                	<h1> ABOUT CARPETCALL </h1>
+                    <p>Carpet Call is Australia’s largest independent flooring retailer stocking and installing all of your flooring needs with our huge range of flooring products. We have over 70 stores Australia wide completely stocked with all types of flooring including carpet, rugs, timber, laminate, vinyl floors, and much more. At Carpet Call you’ll find exactly what you need to brighten up any room in your home. </p>
+                    <p> Our first Carpet Call store opened 40 years ago and since then we have grown to become one of Australia’s most well-known and best loved providers of quality flooring products. We are known for our professional and knowledgeable staff, so visit a Carpet Call store near you for expert advice about your next flooring purchase.</p>
+                    
+                    <div class="rmore rmoree"><a href="#"> Read More </a></div><div class="clearfix"></div>
+                </div>
+                </div><!-- about end here -->
+                
+                <div class="col-md-6"><!-- maps start here -->
+                <div class="stmap">
+                	<h2> VISIT OUR STORES </h2>
+                    
+                    <div class="store-map">
+                    map here !!!
+                    </div><div class="clearfix"></div><!-- for store map end -->
+                    
+                    <div class="rmore rmoree"><a href="#"> Find a Store </a></div><div class="clearfix"></div>
+                </div>
+                </div><!-- maps end here -->
+                
+            </div><div class="clearfix"></div>
+        </div><div class="clearfix"></div>
+       </div><div class="clearfix"></div>
