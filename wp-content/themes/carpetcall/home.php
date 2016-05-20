0aae4f5fc820259d5bfd120151e0538e732cf42c
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-05-20 14:14:42 +0545

    expert content creation

--- wp-content/themes/carpetcall/home.php
+++ wp-content/themes/carpetcall/home.php
@@ -73,56 +73,9 @@
        <!--ideas and advice strats here -->
        <?php get_template_part('content','idea');?>
       <!-- ideas and advice end here -->
-                 
-       <div class="trade"><!-- trade section start here -->
-       		<div class="container">
-            	<div class="hamro_trade">
-                	<h2>we are the experts in the trade </h2>
-                    
-                    <div class="row">
-                    	<div class="col-md-6 idea-left">
-                        <div class="measure_blk">
-                        	<div class="meas_img" style="background:url(<?php echo get_template_directory_uri().'/images/trade-a.jpg';?>); min-width:482px; min-height:267px; background-size:cover;">
-                            <img src="<?php echo get_template_directory_uri().'/images/trade-a.jpg';?>" alt="guide" class="img-responsive"/>
-                            </div><div class="clearfix"></div>
-                           
-                           <div class="measr_cnt">
-                           	<h3><span class="frcol"> FREE</span> MEASURE AND QUOTE </h3>
-                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                            <p> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
-                           </div><div class="clearfix"></div>
-                           
-                           <div class="find_tb find_tbb">
-                          <a href="#"> FIND OUT MORE </a>
-                           </div><div class="clearfix"></div>
-                            
-                        </div>
-                        </div><!-- measuer end -->
-                        
-                        <div class="col-md-6 idea-right">
-                        <div class="measure_blk">
-                        	<div class="meas_img" style="backgroundurl(<?php echo get_template_directory_uri().'/images/trade-b.jpg';?>); min-width:482px; min-height:267px; background-size:cover;">
-                            <img src="<?php echo get_template_directory_uri().'/images/trade-b.jpg';?>" alt="guide" class="img-responsive"/>
-                            </div><div class="clearfix"></div>
-                           
-                           <div class="measr_cnt">
-                           	<h3> CARPETCALL INSTALLATION </h3>
-                            <p> Carpetcall offers flooring installation for our laminate, vinyl, bamboo, cork, timber &amp; carpet products. </p>
-                            <p> One of our Flooring Specialist can meet you in your home, where they can accurately measure and quote on your installation.  </p>
-                           </div><div class="clearfix"></div>
-                           
-                           <div class="find_tb find_tbb">
-                          <a href="#"> CONTACT US </a>
-                           </div><div class="clearfix"></div>
-                            
-                        </div>
-                        </div><!-- installation end -->
-                        
-                    </div><div class="clearfix"></div>
-                    
-                </div><div class="clearfix"></div>
-            </div>
-       </div><div class="clearfix"></div><!-- trade end here -->
+       <!-- trade starts here -->
+         <?php get_template_part('content','expert');?>        
+      <!-- trade end here -->
        
        <?php get_template_part('content','carpetmap');?><!-- about and visit content end here -->
        
