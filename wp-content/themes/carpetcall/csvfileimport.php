a24f5ed3add0543ff98df7605419758fa6857d87
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-05-24 15:38:01 +0545

    data extraction from csv

--- wp-content/themes/carpetcall/csvfileimport.php
+++ wp-content/themes/carpetcall/csvfileimport.php
@@ -5,11 +5,42 @@
 
 $filename = get_template_directory_uri().'/csv/Datawithheaders.csv';
 $file = fopen($filename,"r");
-
+$count_csv = 0;
+$arraycsv = array();
 while(! feof($file))
   {
-echo do_action('pr',(fgetcsv($file)));
+$arraycsv[] =fgetcsv($file);
+$count_csv++;
   }
+  
+echo $count_csv;
+$arraycsvf = array();
+//do_action('pr',$arraycsv);
+$arraycsvKey= array();
+$control=1;
+$arraycsvff = array();
+foreach($arraycsv as $arraycsvu){
+	if($control==1){
+      foreach($arraycsvu as $val){
+      	$arraycsvKey[]=$val;
+      }
 
+	}
+	else{
+		for($i=0;$i<count($arraycsvu);$i++){
+			$arraycsvf[$arraycsvKey[$i]]=$arraycsvu[$i];
+
+		}
+		$arraycsvff[]=$arraycsvf;
+
+	}
+	$control++;
+
+
+}
+
+$testarray[]=array(4,6);
+do_action('pr',$arraycsvff);
+ 
 fclose($file);
 ?>
