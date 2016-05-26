7597c19f685611dc21e73eb114d91890d571cd8f
Chiranjivi Chaudhary <chiranjivi.chaudhary@aits.local>
2016-05-26 09:28:47 +0545

    store finder content and css updated

--- wp-content/themes/carpetcall/content-store.php
+++ wp-content/themes/carpetcall/content-store.php
@@ -19,14 +19,14 @@
                             <!--first sec end  -->
                            
                             <div class="loplc">
-                            <h3>FIND MORE STORES </h3>
+                            <h3>FIND YOUR NEAREST STORE: </h3>
                             
                             <div class="srch_pro">
                             <div class="row">
                                   <div class="col-lg-12">
                                     <form role="search" method="get" id="" class="" action="">
     <div class="input-group">
-      <input id="dir_keyword" name="dir_keyword" type="text" class="form-control" placeholder="Search for..." onkeyup="storecomplet()" autocomplete="off">
+      <input id="dir_keyword" name="dir_keyword" type="text" class="form-control" placeholder="suburb or postcode" onkeyup="storecomplet()" autocomplete="off">
       
       <span class="input-group-btn">
         <button class="btn btn-default" type="button" onclick="autocomplet();"><i class="fa fa-search" aria-hidden="true"></i></button>
