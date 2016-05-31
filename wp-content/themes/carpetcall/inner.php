21353659ed1d8f8ea493a377cda68f61105a70fe
Chiranjivi Chaudhary <chiranjivi.chaudhary@aits.local>
2016-05-31 11:43:12 +0545

    inner page.php content and css updated

--- wp-content/themes/carpetcall/inner.php
+++ wp-content/themes/carpetcall/inner.php
@@ -3,5 +3,10 @@
 * Template Name: inner page
 */?><?php get_header();?>
 
+<div class="container clearfix">
+<div class="inerblock_serc">
+testing phase
+</div>
+</div><div class="clearfix"></div>
 
-<?php get_header();?>
+<?php get_footer();?>
