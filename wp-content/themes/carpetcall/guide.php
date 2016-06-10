9ce0297d5bb88d0707e877536632679cd38fe6ab
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-06-10 14:36:34 +0545

    for guide page sidebar as well guide page

--- wp-content/themes/carpetcall/guide.php
+++ wp-content/themes/carpetcall/guide.php
@@ -6,19 +6,7 @@
 <div class="container clearfix">
 	<div class="inerblock_serc">
 		<div class="col-md-2">
-			<ul class="guide_list " style="color:black;">
-            
-					<?php
-                            $tax = 'guide'; 
-						$tax_terms = get_terms($tax);
-						
-						foreach($tax_terms as $tax_term)
-						{
-						echo '<li><i class="fa fa-caret-right" aria-hidden="true"></i> &nbsp;<a href="'.get_term_link($tax_term).'" style="color:#000000;">'.$tax_term->name.'</li></a>';
-						}
-
-				?>
-            </ul>
+			<?php get_sidebar();?>
             <div class="clearfix"></div>
 		</div>
 		<div class="col-md-10">
--- wp-content/themes/carpetcall/sidebar.php
+++ wp-content/themes/carpetcall/sidebar.php
@@ -1,13 +1,13 @@
-<ul class="guide_list">
+<ul class="guide_list " style="color:black;">
             
 					<?php
-                            $tax = 'faq'; 
+                            $tax = 'guide'; 
 						$tax_terms = get_terms($tax);
 						
 						foreach($tax_terms as $tax_term)
 						{
-						echo '<li><i class="fa fa-caret-right" aria-hidden="true"></i> &nbsp;<a href="'.get_term_link($tax_term).'">'.$tax_term->name.'</li></a>';
+						echo '<li> &nbsp;<a href="'.get_term_link($tax_term).'" style="color:#000000;">'.$tax_term->name.'<i class="fa fa-caret-right" aria-hidden="true"></i></li></a>';
 						}
 
 				?>
-            </ul><div class="clearfix"></div>
+</ul>
