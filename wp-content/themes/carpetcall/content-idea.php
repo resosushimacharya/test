a31300662588c05091db7d46ff55ab9b8db5f9e8
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-06-24 10:37:57 +0545

    customization in link cases..

--- wp-content/themes/carpetcall/content-idea.php
+++ wp-content/themes/carpetcall/content-idea.php
@@ -16,17 +16,30 @@
         <div class="row">
         <?php $guides=get_field('ideas_and_advice_section','option');
         $gi= 2 ;
-        
+        $external_link = null;
         foreach($guides as $guide):?>
              <div class="col-md-6  <?php echo ($gi%2==0?'idea-left':'idea-right') ;?>">
              <div class="<?php echo ($gi%2==0?'rmvisual':'care_pro') ;?>" style="background:url(<?php echo $guide['image']['url'];?>); float:right; background-color:#0a71cf; min-width:483px; min-height:156px; background-size:cover; background-position:center; background-repeat:no-repeat; overflow:hidden;">
-             
+             <?php
+                
+                
+               //do_action('pr',$guide);
+               if(!empty($guide['link_url_external'])){
+               $external_link =$guide['link_url_external'][0]; 
+               }
+               else
+               {
+                $external_link = null;
+                echo "hello";
+               }
+               
+             ?>
              <div class="rmblk_cont">
                 <h3> <?php  echo $guide['title'];?></h3>
                 <p> <?php  echo $guide['description'];?></p>
                 <div class="trynow trynoww">
-                <a href="<?php echo ($guide['link_url_external']!=null?$guide['link_url_external']:$guide['link_url']);?>" target="_blank"> 
-                <?php  echo $guide['link_title'];?> </a><i class="fa fa-angle-right" aria-hidden="true"></i> </div><div class="clearfix"></div>               
+                <a href="<?php echo $guide['link_url']; ?>" <?php if($external_link!=null){ echo 'target = "_blank"';} ?>> 
+                <?php  echo $guide['link_title']; ?> </a><i class="fa fa-angle-right" aria-hidden="true"></i> </div><div class="clearfix"></div>               
             </div>
              </div><div class="clearfix"></div>
              </div>
