6c1d0a9c5a7bb369f6031fa03ad6b76f588d92c8
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-07-01 10:38:27 +0545

    conditional logic made for hanlding the server and local page IDs

--- wp-content/themes/carpetcall/content-carpetmap.php
+++ wp-content/themes/carpetcall/content-carpetmap.php
@@ -1,14 +1,31 @@
+<?php 
+$url = site_url();
+$url =explode('/',$url);
+
+if(strcasecmp($url[2],'localhost')==0){
+  
+$aboutID =317;
+$storeID =1765 ;
+}
+else{
+  
+$aboutID =317;
+$storeID =26768 ;
+}
+
+
+ ?>
 <div class="container"><!-- about and map section start here -->
 <div class="abvist clearfix">
     
     <div class="col-md-6 no-pl"><!-- about start here -->
     <div class="intro">
-        <?php $about = get_post(317);
+        <?php $about = get_post($aboutID);
         ?>
         <h1> <?php echo get_field('home_about_heading','option') ;?></h1>
         <?php echo apply_filters('the_content',$about->post_content); ?>
         
-        <?php $aboutlink = get_field('about_carpetcall_link',317);
+        <?php $aboutlink = get_field('about_carpetcall_link',$aboutID);
         ?>
         <div class="rmore rmoree"><?php echo $aboutlink;?></div><div class="clearfix">  </div>
     </div>
@@ -22,7 +39,7 @@
             
             <?php get_template_part('content','map');?>
             </div><div class="clearfix"></div><!-- for store map end -->
-            <?php $storelink = get_field('visit_our_store_link',878);
+            <?php $storelink = get_field('visit_our_store_link',$storeID);
             ?>
             <div class="rmore rmoree"><?php echo $storelink;?></div><div class="clearfix"></div>
         </div>
