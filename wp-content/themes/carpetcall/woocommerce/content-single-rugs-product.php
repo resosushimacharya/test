3ec16096a0ed1793c3bfb560ec0c39597494adda
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-07-20 08:44:41 +0545

    added modal box in single rugs page

--- wp-content/themes/carpetcall/woocommerce/content-single-rugs-product.php
+++ wp-content/themes/carpetcall/woocommerce/content-single-rugs-product.php
@@ -205,7 +205,31 @@ if ( ! defined( 'ABSPATH' ) ) {
       <div class="cc-product-ship col-md-6"><span>SHIPPING<i class="fa fa-info-circle" aria-hidden="true"></i></span></div><div class="cc-product-free col-md-6"> FREE DELIVERY</div>
       </div>
       <div class="cc-product-pick-location-section col-md-12">
-      <div class="cc-product-pick col-md-6">PICK UP</div><div class="cc-product-location col-md-6"><button type="button" class="btn btn-default col-md-12"> PICK UP LOCATION</button></div>
+      <div class="cc-product-pick col-md-6">PICK UP</div><div class="cc-product-location col-md-6">
+      <button type="button" class="btn btn-default col-md-12" data-toggle="modal" data-target="#myModal">PICK UP LOCATION</button></div>
+      <!-- Trigger the modal with a button -->
+
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
       </div>
 	</div><!-- .summary -->
 
