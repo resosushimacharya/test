b438620a83edf293cbc99d742001abbd6ba74952
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-07-25 15:15:53 +0545

    adde div section for category page

--- wp-content/themes/carpetcall/taxonomy-product_cat.php
+++ wp-content/themes/carpetcall/taxonomy-product_cat.php
@@ -213,6 +213,94 @@ $currentcat = get_queried_object();
 		?>
 		</div></div></div>
 
+	<div class="inerblock_sec_a">
+
+    <div class="container clearfix you_may_link_cntr">
+        <h3 style="text-align:center">YOU MAY ALSO LIKE</h3>
+
+
+
+<?php
+$tax = 'product_cat';
+ ?><?php
+						$tax_terms = get_terms($tax);
+
+					 $args=array(
+					'post_type' => 'product',
+					
+					'post_status' => 'publish',
+					'posts_per_page' => -1,
+					'ignore_sticky_posts'=> 1
+					);
+					//echo $tax_term->slug;
+					$my_query = null;
+					$my_query = new WP_Query($args);
+					while ($my_query->have_posts()) : $my_query->the_post();
+					$woo=get_post_meta($post->ID);
+					
+					$price=$woo['_regular_price'][0];
+					
+					
+					$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+					
+					/*if(!empty(unserialize($woo['_product_attributes'][0])))
+				$prounits=unserialize($woo['_product_attributes'][0]);*/
+				
+				if(isset($prounits['size']['value'])){
+					$prounit=$prounits['size']['value'];
+				}
+               ?>
+                     <?php  if($woo['_featured'][0]=='yes'){ ?>
+                   <div class="col-md-4">
+                  		<div class="pro_secone">
+                  		<div class="img_cntr" style="background-image:url('<?php echo $feat_image; ?>');"></div>
+                  
+                    <!--img src="<?php echo $feat_image; ?>" alt="<?php the_title();?>" class="img-responsive"/-->
+                    <div class="mero_itemss">
+                      		<div class="proabtxt">
+					<h4><?php
+					the_title();?></h4><?php 
+
+					$reqTempTerms=get_the_terms($post->ID,'product_cat');
+					
+
+					
+
+					
+					if(!empty($price)){
+						echo '<h5> FROM A$'.$price.'</h5>';
+						
+						}?></div>
+					<div class="clearfix"></div>
+                        <a href="<?php the_permalink();?>" class="" /> Read More</a>
+                      </div>
+                      </div>
+                      </div>
+                      <?php }?>
+					
+               <?php
+
+					endwhile;
+					wp_reset_query();
+					?><div class="clearfix"></div>
+					
+                    
+    </div>
+    </div>
+    <div class="inerblock_sec_a">
+
+    <div class="container clearfix you_may_link_cntr">
+    <p>
+    	Inspired by the world renowned street art of Berlin; this range is modern vibrant and eye catching. If you want to brighten up your loungeroom this rug centre piece will be your hero.
+
+100% Heat set polypropylene frise; machine made
+
+Non-shedding; highly stain resistant; durable; tight weave; soft under foot; vibrant colour pallete; colour fast; antistatic and easy to clean.
+    </p>
+
+    </div>
+    </div>
+
 <style>
 .cc-img-wrapper a.cc-pro-view {
 	position:absolute;
