4045959f1c908272d7d3916570b789cda13bfca9
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-07-29 09:55:36 +0545

    <br> tag removed il line 105

--- wp-content/themes/carpetcall/taxonomy-product_cat.php
+++ wp-content/themes/carpetcall/taxonomy-product_cat.php
@@ -102,7 +102,7 @@ $currentcat = get_queried_object();
                             	$loopcounter++;
                             	?>
                           <div class="row cc-cat-sub-title-price-cover">
-                            		<div class="col-md-6 cc-cat-sub-title"><h4><?php woocommerce_page_title();?></h4><br />
+                            		<div class="col-md-6 cc-cat-sub-title"><h4><?php woocommerce_page_title();?></h4>
                             		<?php
                             	echo '<h3>'.$discat->name.'</h3><br/>';
                             	 ?>
