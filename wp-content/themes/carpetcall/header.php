fe74fdb3434e028a75edaf1d163306b77454c40e
Bishwa <bishwa.deoja@agileitsolutions.net>
2016-08-13 12:26:14 +0545

    Updated CSS for About Us left navigation styling . Updated code to display correct phone number link .

--- wp-content/themes/carpetcall/header.php
+++ wp-content/themes/carpetcall/header.php
@@ -250,7 +250,7 @@ echo get_field('telephone', '89',false);
 
 ?> </a></h2>
                     <h3 class="subcl"><?php
-echo get_field('contact_label', '89',false);
+echo __( 'CALL', 'carpetcall' ) . get_field('contact_label', '89',false);
 ?>"</h3>
                      <div class="contblk"><a href="<?php
 echo get_field('contact_url', '89');
