6cb20b84aa7b750ae121519d5c780ecafb6f02a4
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-08-15 14:31:20 +0545

    code has been reviewed and indention fixed.

--- wp-content/themes/carpetcall/content-navwoo.php
+++ wp-content/themes/carpetcall/content-navwoo.php
@@ -1,18 +1,24 @@
 <ul class="nav navbar-nav navbar-right" id="mywoosection">
-	<li class="dropdown woocomerce_dropdown" id="woo_control" >
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><div class="mcrt">
-      <img src="<?php echo get_template_directory_uri() ;?>/images/cart-icon.png" alt="icon" width="31" height="25" style="float:left;"/> <span class="crrt"> MY CART </span>
-      <div class="rnkct"> <span class="badge" id="counttest" >
-        <?php $count=0;
-        foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ):
-        $count++;
-        endforeach;
-       echo $count; ?>
+      	<li class="dropdown woocomerce_dropdown" id="woo_control" >
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        <div class="mcrt">
+                             <img src="<?php echo get_template_directory_uri() ;?>/images/cart-icon.png" alt="icon" width="31" height="25" style="float:left;"/>
+                             <span class="crrt"> MY CART </span>
+                             <div class="rnkct">
+                                 <span class="badge" id="counttest" >
+                                  <?php 
+                                          $count=0;
+                                          foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ):
+                                                  $count++;
+                                          endforeach;
+                                          echo $count; 
+                                  ?>
 
-      </span>
+                                  </span>
 
-    </div>
-  </div> </a>
-  <ul class="dropdown-menu" id="cc-mini-cart-cntr"></ul>
-	</li>
-			</ul>
+                              </div>
+                        </div> 
+                </a>
+                <ul class="dropdown-menu" id="cc-mini-cart-cntr"></ul>
+	     </li>
+</ul>
