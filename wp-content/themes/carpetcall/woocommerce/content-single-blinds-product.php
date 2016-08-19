1cd76050c99073b65a390a272c88430c34436fe5
Yamu Aryal <yamu.aryal@agileitsolutions.net>
2016-08-19 14:55:24 +0545

    Carpets and Blinds single page Enquire now modal

--- wp-content/themes/carpetcall/woocommerce/content-single-blinds-product.php
+++ wp-content/themes/carpetcall/woocommerce/content-single-blinds-product.php
@@ -162,7 +162,259 @@ if($reqTempTerms){
       <div class="cc-product-enquiry col-md-12">
       	<button type="button" class="btn btn-default col-md-12" data-toggle="modal" data-target="#myModal2">ENQUIRE NOW</button>
       </div>
+      <div id="myModal2" class="modal fade querynow" role="dialog">
+
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-a-title">ENQUIRE NOW</h4>
+      </div>
+      <div class="modal-body">
+			<div class="review-form">
+            <form action="" method="post" id="contact_form" role="form">
+            <div class="contact_block_cntr">
+            
+                <div class="yd-title"> 	
+                <h3>YOUR  DETAILS</h3> 		
+                </div>
+                <select class="selectpicker col-md-6 valid" name="cc_enquiry_type" id="cc-enquiry-type" aria-invalid="false">
+            
+                       <option class="col-md-12" value="sales enquiry">
+                         Sales Enquiry
+                       </option>
+                       <option class="col-md-12" value="Service Enquiry">
+                         Service Enquiry
+                       </option>
       
+                     </select>
+                <div class="flsm-blk">
+                <div class="form-group col-sm-6"> <label for="first_name">First Name</label>
+                    <input type="text" name="first_name" class="form-control" value="" size="40"  id="first_name"  placeholder="E.G. JOHN">
+                    <div class="error_label"></div> 
+                </div>
+               
+                <div class="form-group col-sm-6"><label for="first_name">Last Name </label>
+                    <input type="text" name="last_name" class="form-control" value="" size="40" id="last_name"  placeholder="E.G. SMITH ">
+                    <div class="error_label"></div>
+                </div>
+                
+                </div>
+                
+                <div class="meroi-blka">
+	            <div class="form-group col-sm-12"><label for="email_address">Email</label>
+	                   <input type="email" name="email_address" class="form-control" value="" size="40"  id="email_address" placeholder="E.G. JOHN.SMITH@EMAIL.COM">
+	                    <div class="error_label"></div>
+	            </div>
+                </div>
+                
+                <div class="tela-blkb">
+	            <div class="form-group col-sm-6">
+	            	<label for="mobile_phone_no">Phone</label>
+                    <input type="tel" name="mobile_phone_no" class="form-control"  value="" size="40" id="mobile_phone_no" placeholder="E.G. 0212345678">
+                    <div class="error_label"></div>
+                </div>
+                </div>
+                
+                
+                
+                
+                <div class="cc-store-form-section" id="cc-store-form-section">
+                <div class="form-group col-sm-12">	
+                		<h3>SELECT A STORE</h3>
+                		<p> Choose a store that is close to you so we can best respond to your query.</p>
+                </div>
+                
+                <div class="provision-section col-sm-12 clearfix">
+                <div class="form-group col-sm-4">
+
+                	<select class="selectpicker col-md-6 form-control"  name="cc_state_type" id="cc-state-type">
+                	<option class="col-md-12" value="default">STATE</option>
+                      <?php  get_template_part('templates/contact/content', 'contact-state');
+                     ?>
+
+                     </select>
+                     <div class="error_label"></div>
+                </div>
+                
+                <div class="form-group col-sm-8">
+              
+                	<select class="selectpicker col-md-6 form-control" name="cc_store_name" id="cc-store-name">
+                     <option class="col-md-12" value="default">Select a Store</option>
+
+                         <?php  get_template_part('templates/contact/content', 'contact-store');
+                     ?>
+
+                     </select>
+                     <div class="error_label"></div>
+                </div>
+
+                </div>
+                  <div class="form-group col-sm-8">
+                 <div class="cc-product-page-info">
+                 <?php  
+                        global $post;
+                        $reqTempTerms=get_the_terms($post->ID,'product_cat');
+
+
+                        if($reqTempTerms){
+
+                        foreach($reqTempTerms as $cat){
+                        $has_sub_cat=get_terms(array('parent'=>$cat->term_id,'taxonomy'=>'product_cat'));
+
+                        if(count($has_sub_cat)==0){
+                          $reserve = $cat->name ;
+
+                        }
+                        }
+                        }
+
+                         
+                        $resproHeight= get_post_meta($post->ID,'_height',true);
+                        $resproLength = get_post_meta( $post->ID,'_length', true);
+                        $resproWidth = get_post_meta( $post->ID,'_width', true);
+                        $resproSKU   = get_post_meta($post->ID,'_sku',true);
+                        $resproSize ='Size : '.$resproLength.'cm'.' '.$resproWidth.' '.'cm'.' '.$resproHeight;
+                        $resproCode = 'Hard Flooring Code : '.$resproSKU;
+                        $resproProduct ='Product : '.$reserve; 
+                        
+                        echo '<input type="hidden" value="'.$resproProduct.'" name="product_page_cat"/>';
+                        echo '<input type="hidden" value="'.$resproCode.'" name="product_page_code"/>';
+                        echo '<input type="hidden" value="'.$resproSize.'" name="product_page_size"/>';
+                        
+
+
+                 ?>
+                   
+                 </div>
+                 </div>
+                
+                </div>
+                
+                
+                <?php 
+                  $myemail = "";
+                  if(isset($_GET['id']))
+                  {
+
+                      $contactstoID = $_GET['id'];
+
+                      $field = get_post_meta($contactstoID);
+                    
+                  if(isset($field['wpsl_email'][0]))
+                  {
+                      $myemail = $field['wpsl_email'][0];
+                  }
+                  else
+                  {
+                      $stateemailpair = array();
+
+
+                      $args = array(
+                                      'post_type' =>'wpsl_stores',
+                                      'posts_per_page'=>'-1',
+                                      'meta_query' => array(
+                                                              array(
+                                                                  'key' => 'store_type',
+                                                                  'value' => 'head_office'
+                                                                  
+                                                              )
+                                                            )
+                                    );
+                      $loop = new WP_Query($args);
+                      $html = '';
+                      if($loop->have_posts())
+                      {
+                          while($loop->have_posts())
+                          {
+                              $loop->the_post();
+                                    /* 
+                                    **state head office  state and 
+                                    ***email address pair 
+                                    
+                                    */
+                                    
+                              $res = get_post_meta($loop->post->ID);
+                                     
+                              if(!isset($res['wpsl_email'][0]))
+                              {
+                                  $stateemailpair[$res['wpsl_state'][0]]  = get_option('admin_email');
+                              }   
+                              else
+                              {
+                                  $stateemailpair[$res['wpsl_state'][0]] = $res['wpsl_email'][0];
+                              }   
+                               
+                          
+                             
+
+                          }
+                          wp_reset_query();
+
+
+                      }
+                   
+                      $myemail =$stateemailpair[$field['wpsl_state'][0]];
+                  }
+                }
+              ?>
+                <input type="hidden" value="<?php echo $myemail;?>" class="btn-dn" id="send_email_address" name="send_email_address">
+                
+                <div class="ur-msg-title">
+                 <div class="form-group col-sm-12">
+                	
+                		<h3>Message</h3>
+                </div></div>
+
+                
+                <div class="form-group col-sm-12">
+                
+                 <textarea class="form-control" rows="5" id="cc_message" name="cc_message" placeholder="ENTER YOUR MESSAGE HERE"></textarea>
+                  <div class="error_label"></div>
+                </div>
+                 <div class="form-group col-sm-12">
+					   <script src='https://www.google.com/recaptcha/api.js'></script>
+                      <div class="g-recaptcha" data-callback="recaptchaCallbackone" data-sitekey="6LdfuCMTAAAAAGhFRMwboqar9gIW_yfmWVjT7OMj"></div>
+                       <input type="hidden" value="" id="check_captcha_one" name="check_captcha_one">
+                      <div class="error_label"></div>
+                   </div>
+                <div class="form-group col-sm-12">
+                 <input type="submit" value="Submit" class="btn-dn" id="cc_contact_submit">
+                </div> 
+                  <div class="form-group col-sm-12 success_message_wrapper ">
+                     
+                     <div class="success_message"></div><div class="close_box">X</div>
+
+                  </div>
+                   <div class="form-group col-sm-12 success_message_wrapper ">
+                     
+                     <div class="error_message"></div><div class="close_box">X</div>
+
+                  </div>
+
+
+            </div>
+
+        </form> 
+		<script>
+        
+ 
+  function recaptchaCallbackone(){
+   jQuery('#check_captcha_one').val('1');
+
+};
+</script>
+
+            </div><div class="clearfix"></div>            
+      </div>
+      
+    </div>
+
+  </div>
+ 
+</div>
       <div class="clearfix"></div>
       
        </div>
