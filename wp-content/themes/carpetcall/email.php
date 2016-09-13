e5f8db92ea61a0d617a344e4c9a45b58dc8ff366
Prakash Shrestha <prakash.shrestha@agileitsolutions.net>
2016-09-13 17:32:57 +0545

    changes in layout

--- wp-content/themes/carpetcall/email.php
+++ wp-content/themes/carpetcall/email.php
@@ -14,9 +14,9 @@
     		<div style="height:35px; background-color:#144a9f; padding:17px 29px; clear:both;">
             
 
-            	<div style="display:block; margin-left:12px;" > <img src="http://resolutionaustralia.com.au/design/Carpetcall/carpetcall-logo.png" width="171" height="35" alt="logo"/></div><!-- logo end -->
+            	
 
-            	<div style="float:left; margin-left:12px;" ><a href="<?php echo site_url();?>" ><img src="<?php echo get_template_directory_uri();?>/images/logo-mail.png" width="171" height="35" alt="logo"/></a></div><!-- logo end -->
+            	<div style="float:left; margin-left:12px;" ><a href="<?php echo site_url();?>" ><img src="http://resolutionaustralia.com.au/design/Carpetcall/carpetcall-logo.png" width="171" height="35" alt="logo"/></a></div><!-- logo end -->
 
                 
                 <div style="float:right; width:179px;">
@@ -156,17 +156,13 @@ OUR FLOORING SPECIALISTS </p>
                 	<ul style="margin:0; padding:0; display:block;">
                     <li style="display:inline-block; font-family:'proxima_nova_rgregular', sans-serif; font-size:6.88px; font-weight:bold; color:#FFF; line-height:9px; text-transform:uppercase;"> Get to know us </li>
 
-                    <li style="display:inline-block; padding-left:10px;"><a href="#" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/facebook.png" width="9" height="9" alt="facebook"/></a></li>
-                    <li style="display:inline-block; padding-left:10px;"><a href="#" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/youtube.png" width="11" height="8" alt="youtube"/></a></li>
-                    <li style="display:inline-block; padding-left:10px;"><a href="#" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/pinterest.png" width="9" height="9" alt="pinterest"/></a></li>
-                    <li style="display:inline-block; padding-left:10px;"><a href="#" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/google-plus.png" width="13" height="9" alt="google-plus"/></a></li>
-                    <li style="display:inline-block; padding-left:10px;"><a href="#" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/instagram.png" width="10" height="9" alt="instagram"/></a></li>
+                  
 
-                    <li style="display:inline-block; padding-left:10px;"><a href="http://www.facebook.com/carpetcallau" style="text-decoration:none;"><img src="<?php echo get_template_directory_uri();?>/images/facebook.png" width="9" height="9" alt="facebook"/></a></li>
-                    <li style="display:inline-block; padding-left:10px;"><a href="http://www.youtube.com/user/carpetcallau?sub_confirmation=1" style="text-decoration:none;"><img src="<?php echo get_template_directory_uri();?>/images/youtube.png" width="11" height="8" alt="youtube"/></a></li>
-                    <li style="display:inline-block; padding-left:10px;"><a href="http://www.pinterest.com/carpetcall" style="text-decoration:none;"><img src="<?php echo get_template_directory_uri();?>/images/pinterest.png" width="9" height="9" alt="pinterest"/></a></li>
-                    <li style="display:inline-block; padding-left:10px;"><a href="https://plus.google.com/108290827729290320654" style="text-decoration:none;"><img src="<?php echo get_template_directory_uri();?>/images/google-plus.png" width="13" height="9" alt="google-plus"/></a></li>
-                    <li style="display:inline-block; padding-left:10px;"><a href="https://www.instagram.com/carpetcallau/" style="text-decoration:none;"><img src="<?php echo get_template_directory_uri();?>/images/instagram.png" width="10" height="9" alt="instagram"/></a></li>
+                    <li style="display:inline-block; padding-left:10px;"><a href="http://www.facebook.com/carpetcallau" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/facebook.png" width="9" height="9" alt="facebook"/></a></li>
+                    <li style="display:inline-block; padding-left:10px;"><a href="http://www.youtube.com/user/carpetcallau?sub_confirmation=1" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/youtube.png" width="11" height="8" alt="youtube"/></a></li>
+                    <li style="display:inline-block; padding-left:10px;"><a href="http://www.pinterest.com/carpetcall" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/pinterest.png" width="9" height="9" alt="pinterest"/></a></li>
+                    <li style="display:inline-block; padding-left:10px;"><a href="https://plus.google.com/108290827729290320654" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/google-plus.png" width="13" height="9" alt="google-plus"/></a></li>
+                    head<li style="display:inline-block; padding-left:10px;"><a href="https://www.instagram.com/carpetcallau/" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/instagram.png" width="10" height="9" alt="instagram"/></a></li>
 
                     </ul>
                 </div><!-- social media end -->
