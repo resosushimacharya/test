8924072e71d28a14e8e92dfdbe56f8681e1961ee
Yamu Aryal <yamu.aryal@agileitsolutions.net>
2016-09-14 15:13:23 +0545

    shema update

--- wp-content/themes/carpetcall/woocommerce/single-product/carpets_blinds/price.php
+++ wp-content/themes/carpetcall/woocommerce/single-product/carpets_blinds/price.php
@@ -22,12 +22,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 
 ?>
-<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-<div class="cc-price-control">
-<h3><span itemprop="priceCurrency" content="AUD">$</span><span class="cc-sale-price-title" itemprop="price" content="<?php echo number_format(esc_attr( $product->get_price() ),2,'.',''); ?>"><?php echo number_format(esc_attr( round($product->get_price()) ),2,'.',''); ?></span><span class="low">/SQM</span> </h3></div>
-<h3 class="online_price_subhead">ONLINE PRICE</h3>
-	<meta itemprop="price" content="<?php echo esc_attr( round($product->get_price()) ); ?>" />
-	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
-	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
 
+<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+  <div class="cc-price-control">
+    <h3><span itemprop="priceCurrency" content="AUD">$</span><span class="cc-sale-price-title" itemprop="price" content="<?php echo number_format(esc_attr( $product->get_price() ),2,'.',''); ?>"><?php echo number_format(esc_attr( round($product->get_price()) ),2,'.',''); ?></span><span class="low">/SQM</span> </h3>
+  </div>
+  <h3 class="online_price_subhead">ONLINE PRICE</h3>
+  <meta itemprop="price" content="<?php echo esc_attr( round($product->get_price()) ); ?>" />
+  <meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
+  <?php /*?><link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" /><?php */?>
 </div>
