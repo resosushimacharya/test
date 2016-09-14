c7556e37f615eef7bdc2d5c6b603fe49afe74399
AITS\chiranjivi.chaudhary <chiranjivi.chaudhary@gmail.com>
2016-09-14 18:17:37 +0545

    updated emd - edm email

---
+++ wp-content/themes/carpetcall/edm-email.php
@@ -0,0 +1,211 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Thank You For Your Order</title>
+</head>
+
+<body style="margin:0; padding:0; font-family:Arial, Helvetica, sans-serif;">
+
+<table width="600px" border="0" cellspacing="0" cellpadding="0" style="margin:23px auto;">
+  <tr style="background-color:#144a9f; height:69px;">
+  
+    <td style="width:395px;"><div><a href="<?php echo site_url();?>" style="margin-left:12px;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/carpetcall-logo.png" width="171" height="35" alt="logo"/></a></div></td>
+    
+    <td style="width:205px;"><table width="205px" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td colspan="2"><div style="font-family:'proxima_nova_ltsemibold', sans-serif; font-size:20px; color:#FFF;  line-height:1; clear:both; font-weight:bold;">CALL 1300 502 427 </div></td>
+        </tr>
+      <tr>
+        <td style="width:107px;"><p style=" font-family:'proxima_nova_ltsemibold', sans-serif; font-size:6.69px; color:#FFF; margin:2px 0; text-transform:uppercase;"> OR BOOK A CALL BACK WITH <br>
+OUR FLOORING SPECIALISTS </p> </td>
+        <td style="width:75px; height:14px; background-color:#c32428;"><div style="width:74px; text-align:center;"> <a href="#" style="font-family:'proxima_nova_rgregular', sans-serif; font-size:7.25px; color:#FFF; text-align:center; text-decoration:none; text-transform:uppercase; display:block; padding:5px 14px;"> CONTACT US </a> </div></td>
+        <td></td>
+      </tr>
+    </table></td>
+  </tr> <!-- header section end -->
+  
+  <tr>
+    <td colspan="2" style="border:1px solid #cfcfcf; border-top:0; border-bottom:0; background-color:#f0f2f1;">
+    <div style="background-color:#f0f2f1;  font-family: 'proxima_nova_ltsemibold', sans-serif; font-size:20px; color:#15489f; text-transform:uppercase; font-weight:bold; margin:29px 0 25px 45px;"> THANK YOU FOR YOUR ORDER </div>
+    </td>
+  </tr><!-- thank you for order end -->
+  
+  <tr>
+    <td colspan="2" style="border:1px solid #cfcfcf; border-top:0; border-bottom:0;">
+    <table width="504px" border="0" cellspacing="0" cellpadding="0" style="width:504px; margin:0 49px 0 45px;">
+      <tr style="border:0;">
+        <td colspan="2"><p style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666666; margin-top:29px; clear:both;">Your order has been received and is now being processed. Your order details are shown below for your reference. </p></td>
+        </tr>
+        
+      <tr>
+        <td style="width:252px;">
+        <h1 style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#15489f; font-weight:bold; margin:30px 0 10px 0; clear:both;"> CUSTOMER DETAILS </h1>
+<pre style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:10px; color:#15489f; line-height:13px; margin:0;">
+<span style="font-weight:bold;">Email:</span> email@email.com
+<span style="font-weight:bold;">Tel:</span> (02) XXXX XXXX
+<span style="font-weight:bold;">Order:</span> #XXXXX
+<span style="font-weight:bold;">Order Date:</span> DD/MM/YYYY
+</pre>
+
+        
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+      
+      <tr>
+        <td style="width:252px;">
+        <h2 style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666666; margin:35px 0 0 0; text-transform:uppercase;"> Shipping Address</h2>
+                        <span style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666666; margin:0;">Your order has been delivered to: </span>
+                        <div style="border-bottom:1px solid #e7edf8; margin:5px 20% 10px 0;"></div>
+<pre style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:10px; color:#666; line-height:13px; text-decoration:none; margin:0;">
+[First Name] [Last Name]
+Address Line 1
+Address Line 2
+Suburb State Postcode
+Australia
+</pre>
+                       
+        </td>
+        <td style="width:252px;">
+        <h2 style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666666; margin:35px 0 0 0; text-transform:uppercase;"> Billing Address</h2>
+                        <span style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666666; margin:0;">Your order will be billed to: </span>
+                        <div style="border-bottom:1px solid #e7edf8; margin:5px 20% 10px 0; "></div>
+<pre style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:10px; color:#666; line-height:13px; text-decoration:none; margin:0;">
+[First Name] [Last Name]
+Address Line 1
+Address Line 2
+Suburb State Postcode
+Australia
+</pre> 
+        </td>
+      </tr>
+      
+      <tr>
+        <td colspan="2" style="padding-top:30px;">
+        <table width="504" border="0" cellspacing="0" cellpadding="0" style="width:504px;">
+                      <tr style="background-color:#e7edf8;">
+                        <td width="171" style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666; text-transform:uppercase; padding:12px 15px; font-weight: bold;">Product</td>
+                        <td width="30" style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666; text-transform:uppercase; padding:12px 15px; font-weight: bold;">Qty</td>
+                        <td width="158" style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666; text-transform:uppercase; padding:12px 15px; font-weight: bold;">Price</td>
+                        <td width="145" style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666; text-transform:uppercase; padding:12px 15px; font-weight: bold;">Total</td>
+                      </tr>
+                      <tr>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; border-bottom:1px solid #e7edf8;">AMO: 1524.80.254 <span style="display:block;">SKU: AMO.1524.80254</span></td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; border-bottom:1px solid #e7edf8;">1</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; border-bottom:1px solid #e7edf8;">$4,276.00</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; border-bottom:1px solid #e7edf8;">$4,276.00</td>
+                      </tr>
+                      <tr>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; border-bottom:1px solid #e7edf8;">AMO: 1524.80.254 <span style="display:block;">SKU: AMO.1524.80254</span></td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; border-bottom:1px solid #e7edf8;">1</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; border-bottom:1px solid #e7edf8;">$4,276.00</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; border-bottom:1px solid #e7edf8;">$4,276.00</td>
+                      </tr>
+                      <tr>
+                        <td colspan="2" rowspan="3">&nbsp;</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; text-transform:uppercase; font-weight:bold;">subtotal</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px;">$8,552.00</td>
+                      </tr>
+                      <tr>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; text-transform:uppercase; font-weight:bold;">shipping</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px;">Store pickup</td>
+                      </tr>
+                      <tr>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; text-transform:uppercase; font-weight:bold;">PAYMENT METHOD</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px;">Securepay credit card</td>
+                      </tr>
+                      <tr style="background-color:#e7edf8;">
+                        <td colspan="2" style="background-color: #e7edf8; padding:12px 15px;">&nbsp;</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px; text-transform:uppercase; font-weight:bold;">TOTAL</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;  padding:12px 15px;">$8,552.00</td>
+                      </tr>
+                    </table>
+                    
+        </td>
+        </tr>
+        
+      <tr>
+        <td colspan="2">
+        <div style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666; text-transform:uppercase; border-bottom:1px solid #e7edf8; padding-bottom:8px; margin-bottom:6px; margin-top:36px;">ADDITIONAL COMMENTS OR INSTRUCTIONS</div>
+                <span style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:12px; color:#666;">Instruction </span>
+        </td>
+        </tr>
+        
+      <tr>
+        <td colspan="2">&nbsp;</td>
+      </tr>
+      
+    </table>
+    </td>
+  </tr>
+
+  <tr>
+    <td colspan="2" style="border:1px solid #cfcfcf; border-top:0; background-color:#144a9f;">
+    	<table width="554px" border="0" cellspacing="0" cellpadding="0" style="width:554px; margin:18px 22px 26px 22px;">
+      <tr>
+        <td><a href="<?php echo site_url();?>"><img src="http://resolutionaustralia.com.au/design/Carpetcall/carpetcall-logo.png" width="171" height="35" alt="logo"/></a></td>
+      </tr>
+      
+      <tr>
+        <td>
+        <div style="margin-top:30px;">
+                    <table width="554px" border="0" cellspacing="0" cellpadding="0" style="width:554px;">
+                      <tr>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:8px; color:#FFF;">SHOP OUR RANGE</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:8px; color:#FFF;">IDEAS AND ADVICE</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:8px; color:#FFF;">CLEARANCE</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:8px; color:#FFF;">ABOUT CARPETCALL</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:8px; color:#FFF;">ROOM VISUALISER</td>
+                        <td style="font-family:Arial, Gotham, 'Helvetica Neue', Helvetica, sans-serif; font-size:8px; color:#FFF;">STORE FINDER</td>
+                      </tr>
+                    </table>
+        </div>
+        </td>
+      </tr><!-- menu footer end -->
+      
+      <tr>
+        <td>                  
+        <table width="554px" border="0" cellspacing="0" cellpadding="0" style="margin-top:25px;">
+          <tr>
+            <td style="width:68px; font-family:'proxima_nova_rgregular', sans-serif; font-size:6.88px; font-weight:bold; color:#FFF; text-transform:uppercase;"> Get to know us</td>
+            
+            <td style="width:20px;"><a href="http://www.facebook.com/carpetcallau" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/facebook.png" width="9" height="9" alt="facebook"/></a></td>
+            
+            <td style="width:20px;"><a href="http://www.youtube.com/user/carpetcallau?sub_confirmation=1" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/youtube.png" width="11" height="8" alt="youtube"/></a></td>
+            
+            <td style="width:20px;"><a href="http://www.pinterest.com/carpetcall" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/pinterest.png" width="9" height="9" alt="pinterest"/></a></td>
+            
+            <td style="width:20px;"><a href="https://plus.google.com/108290827729290320654" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/google-plus.png" width="13" height="9" alt="google-plus"/></a></td>
+            
+            <td><a href="https://www.instagram.com/carpetcallau/" style="text-decoration:none;"><img src="http://resolutionaustralia.com.au/design/Carpetcall/instagram.png" width="10" height="9" alt="instagram"/></a></td>
+            
+          </tr>
+        </table>
+        
+        </td>
+      </tr><!-- social icon end -->
+      
+      <tr>
+        <td>
+        
+         <table width="554px" border="0" cellspacing="0" cellpadding="0" style="height:18px; padding: 12px 0 15px 0;">
+           <tr>
+             <td style="width:124px;"><a href="#" style=" font-family:'proxima_nova_rgregular', sans-serif; font-size:6.88px; color:#FFF; text-decoration:none; text-transform:uppercase; border-right: 1px solid #fff; padding-right: 5px;"> © Copyright 2016 Carpet CalL </a></td>
+             <td style="width:45px;"><a href="<?php echo site_url();?>/sitemap/" style=" font-family:'proxima_nova_rgregular', sans-serif; font-size:6.88px; color:#FFF; text-decoration:none; text-transform:uppercase; border-right: 1px solid #fff; padding-right: 5px; margin-left:5px;"> SITE MAP </a></td>
+             <td><a href="<?php echo site_url();?>/terms-and-conditions/" style=" font-family:'proxima_nova_rgregular', sans-serif; font-size:6.88px; color:#FFF; text-decoration:none; text-transform:uppercase; margin-left:5px;"> TERMS AND CONDITIONS </a></td>
+           </tr>
+         </table>
+        
+        </td>
+      </tr><!-- footer end -->
+      
+    </table>
+    </td>
+  </tr>
+</table>
+
+
+
+</body>
+</html>
