f0c64937ebb4b5c8a6c40bb2231ffe63c7088256
Yamu Aryal <yamu.aryal@agileitsolutions.net>
2016-09-19 12:12:17 +0545

    code cleanup

--- wp-content/themes/carpetcall/templates/square-meter-calculator.php
+++ wp-content/themes/carpetcall/templates/square-meter-calculator.php
@@ -1,135 +1,102 @@
 <div id="myModalcalc" class="modal fade smc_calc_cntr" role="dialog">
-  <div class="modal-dialog">
-
+  <div class="modal-dialog"> 
+    
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-      
-
-        
-            <div class="cc-smc-blk-title">
-                <h3 >Square Meter Calculator</h3>
-            </div>
-        
+        <div class="cc-smc-blk-title">
+          <h3 >Square Meter Calculator</h3>
+        </div>
       </div>
       <div class="modal-body clearfix">
-  
-
-
-            
-    
-       <?php
+        <?php
        $x = get_field('size_m2',get_the_ID());
        $stock = get_post_meta(get_the_ID(), '_stock', true);
        ?>
-            <div class="col-md-12 cc-calculator-sec-r">
-            <div class="col-md-6">
-                 <input type="hidden" class="form-control" id="cov_per_pack" placeholder="" name="cov_per_pack" value='<?php echo $x ;?>'>
-                  <input type="hidden" class="form-control" id="cc_Stock_count" placeholder="" name="cc_Stock_count" value='<?php echo $stock;?>'>
-                 </div><div class="col-md-6"></div>
-            </div>
-        
-         
+        <div class="col-md-12 cc-calculator-sec-r">
+          <div class="col-md-6">
+            <input type="hidden" class="form-control" id="cov_per_pack" placeholder="" name="cov_per_pack" value='<?php echo $x ;?>'>
+            <input type="hidden" class="form-control" id="cc_Stock_count" placeholder="" name="cc_Stock_count" value='<?php echo $stock;?>'>
+          </div>
+          <div class="col-md-6"></div>
+        </div>
         <div class="calculator_container" id="calculator_container">
-        <div class="row single-room-sec" id="row_cal_1">
+          <div class="row single-room-sec" id="row_cal_1">
             <div class="col-md-8 col-item-price">
-                <div class="cal_pro" id="cal_pro_1">
-                    <div class="container_1">
-                        <div class="form-group col-md-6">
-                            <div class="col-md-6 no-lr">
-                                <label for="width_1">Room 1</label>
-                            </div>
-                            <div class="col-md-6 no-lr">
-                                <input type="text" class="form-control width_check" id="width_1" placeholder=" Width (m)" name="width_1" required>
-                            </div>
-                        </div>
-                        <div class="form-group col-md-6 for-length">
-                            <div class="col-md-6 no-lr">
-                                <label for="length_1"></label>
-                            </div>
-                            <div class="col-md-6 no-lr">
-                                <input type="text" class="form-control length_check" id="length_1" placeholder="Length (m)" name="length_1" required>
-                            </div>
-                        </div>
+              <div class="cal_pro" id="cal_pro_1">
+                <div class="container_1">
+                  <div class="form-group col-md-6">
+                    <div class="col-md-6 no-lr">
+                      <label for="width_1">Room 1</label>
                     </div>
+                    <div class="col-md-6 no-lr">
+                      <input type="text" class="form-control width_check" id="width_1" placeholder=" Width (m)" name="width_1" required>
+                    </div>
+                  </div>
+                  <div class="form-group col-md-6 for-length">
+                    <div class="col-md-6 no-lr">
+                      <label for="length_1"></label>
+                    </div>
+                    <div class="col-md-6 no-lr">
+                      <input type="text" class="form-control length_check" id="length_1" placeholder="Length (m)" name="length_1" required>
+                    </div>
+                  </div>
                 </div>
-               
+              </div>
             </div>
             <div class="col-md-3 ">
-                        <div class="form-group col-md-8">
-                           <span class="form-control col-md-8 item_indivisual_total"> = <span id="item_total_1">-</span> SQM</span>
-                        </div>
+              <div class="form-group col-md-8"> <span class="form-control col-md-8 item_indivisual_total"> = <span id="item_total_1">-</span> SQM</span> </div>
             </div>
-        </div>
- </div>
-    
-<div class="row smc_calc_top_btns">
- <div class="form-group col-md-8 col-item-price">
-                    <button type="button" class="btn btn-default" id="cal_more">+ Add Rooms</button>
-                    <button type="submit" class="btn btn-default" id="cal_id" >Calculate</button>
-                </div><div class="form-group col-md-4 "></div> </div>
-                </form>
-
-    <div class="row calc-info-cntr excess-cntr">
-      <div class="form-group col-md-8 col-item-price">
-          <p class="pull-right">Excess:</p>
-      </div>
-      <div class="col-md-2 col-total-item-section">
-          <div class="form-group col-md-4 no-lr cc-me">
-             
-               <span class="form-control col-result-input" id="exceess_area_percent"> </span>
           </div>
-          <div class="form-group col-md-4 no-pl">
-          <span class="cc-cal-showup">%</span></div>
-      </div>
-    </div>
-    <div class="row calc-info-cntr total-cntr">
-      <div class="form-group col-md-8 col-item-price">
-          <p class="pull-right">Total:</p>
-      </div>
-      <div class="col-md-2 col-total-item-section">
-          <div class="form-group col-md-4 no-lr cc-me">
-              
-              <span class="form-control col-result-input" id="total_area"> </span>
+        </div>
+        <div class="row smc_calc_top_btns">
+          <div class="form-group col-md-8 col-item-price">
+            <button type="button" class="btn btn-default" id="cal_more">+ Add Rooms</button>
+            <button type="submit" class="btn btn-default" id="cal_id" >Calculate</button>
           </div>
-          <div class="form-group col-md-4 no-pl">
-          <span class="cc-cal-showup">SQM</span></div>
-      </div>
-    </div>
-    <div class="row calc-info-cntr package-cntr">
-      <div class="form-group col-md-8 col-item-price">
-          <p class="pull-right">Total Packs Required:</p>
-      </div>
-      <div class="col-md-2 col-total-item-section">
-        <div class="form-group col-md-4 no-lr cc-me">              
-          <span class="form-control col-result-input" id="no_of_packs"> </span>
+          <div class="form-group col-md-4 "></div>
+        </div>
+        </form>
+        <div class="row calc-info-cntr excess-cntr">
+          <div class="form-group col-md-8 col-item-price">
+            <p class="pull-right">Excess:</p>
+          </div>
+          <div class="col-md-2 col-total-item-section">
+            <div class="form-group col-md-4 no-lr cc-me"> <span class="form-control col-result-input" id="exceess_area_percent"> </span> </div>
+            <div class="form-group col-md-4 no-pl"> <span class="cc-cal-showup">%</span></div>
+          </div>
+        </div>
+        <div class="row calc-info-cntr total-cntr">
+          <div class="form-group col-md-8 col-item-price">
+            <p class="pull-right">Total:</p>
+          </div>
+          <div class="col-md-2 col-total-item-section">
+            <div class="form-group col-md-4 no-lr cc-me"> <span class="form-control col-result-input" id="total_area"> </span> </div>
+            <div class="form-group col-md-4 no-pl"> <span class="cc-cal-showup">SQM</span></div>
+          </div>
+        </div>
+        <div class="row calc-info-cntr package-cntr">
+          <div class="form-group col-md-8 col-item-price">
+            <p class="pull-right">Total Packs Required:</p>
+          </div>
+          <div class="col-md-2 col-total-item-section">
+            <div class="form-group col-md-4 no-lr cc-me"> <span class="form-control col-result-input" id="no_of_packs"> </span> </div>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="row calc-bottom-btn-cntr">
+          <div class="col-md-5 no-pr">
+            <button class="btn btn-default pull-right" type="button" id="cancel_calc"  > Cancel </button>
+          </div>
+          <div class="col-md-6 no-pl">
+            <button class="btn btn-default" type="button" id="confirm_calc" > Confirm </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12 " id="error_max_msg"></div>
         </div>
       </div>
     </div>
-
-
-    <div class="clearfix"></div>
-    <div class="row calc-bottom-btn-cntr">
-      <div class="col-md-5 no-pr">
-        <button class="btn btn-default pull-right" type="button" id="cancel_calc"  >
-           Cancel
-        </button>
-      </div>
-      <div class="col-md-6 no-pl">
-        <button class="btn btn-default" type="button" id="confirm_calc" >
-          Confirm
-        </button>
-      </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-12 " id="error_max_msg"></div>
-    </div>
-            
-            
-      </div>
-      
-    </div>
-
   </div>
 </div>
--- wp-content/themes/carpetcall/templates/template-clearance.php
+++ wp-content/themes/carpetcall/templates/template-clearance.php
@@ -7,88 +7,73 @@
 get_header();
 ?>
 
- <div class="cbg_blk cc-wrapper-blk clearfix">
- <div class="container">
-<div class="inerblock_clearn cc-clearance-whole">
-					
- <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
-    <?php if(function_exists('bcn_display'))
+<div class="cbg_blk cc-wrapper-blk clearfix">
+  <div class="container">
+    <div class="inerblock_clearn cc-clearance-whole">
+      <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
+        <?php if(function_exists('bcn_display'))
     {
         bcn_display();
     }?>
-
-</div>
- <?php while(have_posts()){
+      </div>
+      <?php while(have_posts()){
              	the_post();
              	?>
-             	<h3><?php echo get_the_title();?></h3>
-             
-             		<?php the_content();?>
-             	
-            <?php  }
+      <h3><?php echo get_the_title();?></h3>
+      <?php the_content();?>
+      <?php  }
             wp_reset_query();?>
-
-
+    </div>
+  </div>
 </div>
-</div>
-</div>
-
-
- <div class="container clearfix">
-	<div class="inerblock_clernn">
-		
-		
-			<div class="cbg_content cc-clearance-wrapper clearfix">
-            <?php 
+<div class="container clearfix">
+  <div class="inerblock_clernn">
+    <div class="cbg_content cc-clearance-wrapper clearfix">
+      <?php 
 
              	$rows = get_field('clearance__posts',$post->ID);
                 if( $rows ) {
                     $i = 1;
                     $lencount = count($rows);
              	foreach($rows as $row):?>
-
-             	<div class="col-md-12 cc-clearance-row <?php echo ($i==$lencount)?"cc-mod-bord-feat":"";?>">
-             	<?php
+      <div class="col-md-12 cc-clearance-row <?php echo ($i==$lencount)?"cc-mod-bord-feat":"";?>">
+        <?php
                       if(strcasecmp($row['ad_type'],'video')!=0){?>
-                   	<div class="col-md-4 cc-clearance-left">
-                      <img src="<?php echo $row['featured_image']['url'];?>"/ class="img-responsive">
-                      <?php if($row['sticker']){?>
-<span class="cc-clearance-overlay"><?php echo $row['sticker'];?></span><?php 
+        <div class="col-md-4 cc-clearance-left"> <img src="<?php echo $row['featured_image']['url'];?>"/ class="img-responsive">
+          <?php if($row['sticker']){?>
+          <span class="cc-clearance-overlay"><?php echo $row['sticker'];?></span>
+          <?php 
 }?>
-                      </div>
-                  <?php }
+        </div>
+        <?php }
                    else{ ?>
-                   <?php $videolink = 'https://www.youtube.com/embed/'.$row['featured_video'];?>
-                     	<div class="col-md-4 cc-clearance-left"> <iframe width="100%" height="auto"
+        <?php $videolink = 'https://www.youtube.com/embed/'.$row['featured_video'];?>
+        <div class="col-md-4 cc-clearance-left">
+          <iframe width="100%" height="auto"
 src="<?php echo $videolink;?>
-">
-</iframe>
-<?php if($row['sticker']){?>
-<span class="cc-clearance-overlay"><?php echo $row['sticker'];?></span><?php 
+"> </iframe>
+          <?php if($row['sticker']){?>
+          <span class="cc-clearance-overlay"><?php echo $row['sticker'];?></span>
+          <?php 
 }?>
-</div>
-<?php
+        </div>
+        <?php
                    }
                    ?>
-                    <div class="col-md-8 cc-clearance-right">
-                    	<h4 ><?php echo $row['title'];?></h4>
-                    	<p><?php echo $row['description'];?></p>
-                    	<?php 
+        <div class="col-md-8 cc-clearance-right">
+          <h4 ><?php echo $row['title'];?></h4>
+          <p><?php echo $row['description'];?></p>
+          <?php 
                     	$win = $row['window']; 
                     	if(strcasecmp($row['ad_type'],'doc')!=0){?>
-                    	<?php $link=null;
+          <?php $link=null;
                         if($row['link_url'] && $row['link_title']){
                         $link = $row['link_url'];	
                         $title = $row['link_title'];  
                         ?>
-                        <div class="nowspe nowsppe pull-right">
-                        <a href="<?php echo $link;?>" <?php echo (strcasecmp($win, 'yes')== 0)?'target="_blank"':null;?>>
-                            <?php echo $title;?>
-                        </a>
-                        </div>
-                        <?php } ?>
-                    	
-                    <?php } else 
+          <div class="nowspe nowsppe pull-right"> <a href="<?php echo $link;?>" <?php echo (strcasecmp($win, 'yes')== 0)?'target="_blank"':null;?>> <?php echo $title;?> </a> </div>
+          <?php } ?>
+          <?php } else 
                     {
                             $win = $row['window']; 
                             $doc_url = $row['featured_document']['url'];
@@ -96,34 +81,20 @@ src="<?php echo $videolink;?>
 
                             if( $doc_url && $title ) {
                     	?>
-	                    <div class="nowspe nowsppe pull-right">
-	                    <a href="<?php echo $doc_url; ?>" target="_blank">
-	                     <?php echo $title;?> 
-	                     </a>
-	                     </div>
-
-                    <?php 
+          <div class="nowspe nowsppe pull-right"> <a href="<?php echo $doc_url; ?>" target="_blank"> <?php echo $title;?> </a> </div>
+          <?php 
                             }#end-if
                         }#end-else
                     ?>
-
-                    </div>
-                   
-</div>
-<?php $i++; ?>
-
-            <?php  endforeach;
+        </div>
+      </div>
+      <?php $i++; ?>
+      <?php  endforeach;
         }# end-if
 			?>
-
-
-
-
-             
-		</div>
+    </div>
+  </div>
 </div>
-</div>
-
 <?php 
 get_footer();
 ?>
