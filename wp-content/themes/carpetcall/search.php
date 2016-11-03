fc171156c6492f945fd0cf438cef815060a4643e
Yamu Aryal <yamu.aryal@agileitsolutions.net>
2016-11-03 13:50:13 +0545

    CSFR Fix by using nounce in all product enquiries and search form

--- wp-content/themes/carpetcall/search.php
+++ wp-content/themes/carpetcall/search.php
@@ -1,5 +1,11 @@
 
 <?php get_header( 'shop' );?>
+<?php
+$nonce = $_REQUEST['_wpnonce'];
+if ( ! wp_verify_nonce( $nonce, 'search_nounce' ) ) {
+  exit; // Get out of here, the nonce is rotten!
+}
+?>
 <div class="child-innerpg">
     <div class="container clearfix">
         <div class="inerblock_serc_child about-page cc-search-allprd">
@@ -12,7 +18,9 @@
                             <link itemprop="url" href="<?php echo site_url()?>"/>
                             <form name="search-form" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction" method="get" action="<?php echo home_url()?>">
                                   <meta itemprop="target" content="<?php echo site_url()?>?q={<?php echo get_search_query()?>}"/>
-                              <div class="input-group"><input itemprop="query-input" type="text" name="s" placeholder="TYPE TO SEARCH" class="form-control" value="<?php echo get_search_query()?>"/>
+                              <div class="input-group">
+                              <?php wp_nonce_field( 'search_nounce' ); ?>
+                              <input itemprop="query-input" type="text" name="s" placeholder="TYPE TO SEARCH" class="form-control" value="<?php echo get_search_query()?>"/>
                               <span class="input-group-btn">
     <button type="submit" class="btn btn-default cc_search_button" id="searchsubmit" value="">
       <svg xmlns="http://www.w3.org/2000/svg" width="626" height="626" viewBox="0 0 626 626" fill="#808080"><path d="M234.502 468.998C105.024 469.033-.248 364.54.162 234.145.572 103.867 104.737.123 234.957.16 365.233.194 469.347 105.7 469.03 235.294c-.316 129.3-105.033 233.77-234.528 233.703zM58.722 235.14c.39 97.628 79.406 175.614 175.932 175.53 97.425-.083 176.05-79.147 176.148-175.985.098-96.948-79.577-176.324-176.626-175.965-97.164.36-175.857 79.485-175.455 176.42zm323.766 229.716c33.232-21.7 60.585-48.976 83.233-83.725 2.076 2.764 3.414 5.066 5.23 6.89 45.408 45.505 90.928 90.897 136.274 136.462 14.354 14.424 21.37 31.882 17.53 52.252-4.316 22.89-17.586 38.725-40.127 45.99-22.942 7.392-42.994 1.375-59.6-15.042-47.256-46.715-94.08-93.866-141.07-140.852-.344-.345-.593-.788-1.468-1.972z"></path></svg>
